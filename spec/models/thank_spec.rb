cad1b3084eaa06471d48c2eecf7c17c3170debbd
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2020-02-05 11:04:41 +0900

    update thank_spec.rb

--- spec/models/thank_spec.rb
+++ spec/models/thank_spec.rb
@@ -12,30 +12,20 @@ RSpec.describe Thank, type: :model do
 
     context 'can not save' do
 
-      it 'is invalid without text and sender and receiver' do
-        thank = build(:thank, text: nil, receiver_id: nil, sender_id: nil)
-        thank.valid?
-        expect(thank.errors[:sender]).to include("を入力してください")
-        expect(thank.errors[:receiver]).to include("を入力してください")
-        expect(thank.errors[:text]).to include("を入力してください")
-      end
-
-      it 'is invalid without text and sender' do
+      it 'is invalid without sender' do
         receiver = create(:user)
         sender = create(:user)
-        thank = build(:thank, text: nil, receiver_id: receiver.id, sender_id: nil)
+        thank = build(:thank, receiver_id: receiver.id, sender_id: nil)
         thank.valid?
         expect(thank.errors[:sender]).to include("を入力してください")
-        expect(thank.errors[:text]).to include("を入力してください")
       end
 
-      it 'is invalid without text and receiver' do
+      it 'is invalid without receiver' do
         receiver = create(:user)
         sender = create(:user)
         thank = build(:thank, text: nil, receiver_id: nil, sender_id: sender.id)
         thank.valid?
         expect(thank.errors[:receiver]).to include("を入力してください")
-        expect(thank.errors[:text]).to include("を入力してください")
       end
 
       it 'is invalid without text' do
