185f9ba4fb16ca511b7bc3c4cf90bc33040b631a
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2020-02-11 02:18:29 +0900

    user_search function(add user_id)
    検索時にuser_idを一緒に仕込む機能を実装。
    inputタグを仕込むためにlocalStrageに保存する。
    検索するたびに初期化する。

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
   $("#search_form").autocomplete({
     source: function (request, response) {
+      // 仕込みinputの初期化
+      var details = document.getElementById("details")
+      var details_child = details.childNodes[0];
+      if (details_child){
+        details.removeChild(details_child);
+      }
+      // 
       var suggests = [];
       $.ajax({
         url: "/search/users/index",
@@ -9,24 +16,54 @@ document.addEventListener('DOMContentLoaded', function(){
       })
       .done(function(users){
         users.forEach( function(user){
-          console.log(user);
-
           // グループ名を検索時に表示させる。
           // 同一名の人が検索できてしまうことを考えると顔写真付きの方がいいかもしれない
-          //
-          user_profile = `${user.name} : ${user.id}`; 
-          //
+          user_profile = { label: user.name,  value: user.name } 
           //
           //
-          suggests.push( user_profile );
-          console.log( suggests );
-          response( suggests );
+
+          // 
+          // 以下localStrageへの保存方法
+          var setjson_user = JSON.stringify(user);
+          localStorage.setItem(user.name, setjson_user);
+          // 
+          // 
+          
+          // 
+          // 検索結果をHTMLに返す
+          suggests.push( user_profile);
+          return response(suggests);
+          // 
+          // 
         });
       })
     },
     autoFocus: true,
     delay: 300,
-    minLength: 1
-  });
+    minLength: 1,
+    select: function(e, ui){
+      if (ui.item) {
 
+        // 仕込み用のinputタグ生成。user_idを渡す。
+        // マツケンさんへ
+        // 検索フォームを作成するときにformタグの中に<span id="details"></span>を入れて欲しいです。
+        // 仕込みのinputタグを挿入しているので
+        var params = document.createElement("input");
+        params.setAttribute("type", "hidden");
+        params.setAttribute("name", "user[:id]");
+        params.setAttribute("class", "search_user_id");
+        params.setAttribute("data-name", ui.item.label);
+        // localStrageから取得。jsonで扱えるようにするt前にparseメソッドで変換する。
+        var getjson_user = localStorage.getItem(ui.item.value);
+        var user_details = JSON.parse(getjson_user);
+        // 
+        params.value = user_details.id;
+        document.getElementById("details").appendChild(params);
+        // 
+        // 
+        // 
+        // 
+      }
+    }
+  });
 }, false);
