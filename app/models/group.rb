683816d04386dc04f89ca8abd2d6f9cf7b103f5d
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2020-02-12 17:56:40 +0900

    search_group function method
    グループを検索するための機能実装
    groupモデルに記述追加点
    ・ユニット、グループ、チーム、ランクを検索する機能。

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,33 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   has_ancestry
+
+  def self.search_units
+    units = Group.where(ancestry: nil)
+    return units
+  end
+
+  def self.search_groups
+    units = Group.search_units
+    groups = []
+    units.each do |unit|
+      unit_have_groups = Group.children_of unit
+      unit_have_groups.each do |group|
+        groups.push(group)
+      end
+    end
+    return groups
+  end
+
+  def self.search_teams
+    groups = Group.search_groups
+    teams = []
+    groups.each do |group|
+      group_have_teams = Group.children_of group
+      group_have_teams.each do |team|
+        teams.push(team)
+      end
+    end
+    return teams
+  end
 end
