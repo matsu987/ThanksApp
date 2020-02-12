8ba0ca4eff1fbfdda1048ba80330b874b59cb418
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2020-02-12 17:55:33 +0900

    search_group function js
    グループを検索するための機能実装
    jsでの記述追加点
    ①ユニット、グループ、チーム、ランクをajaxでデータベースから持ってくる。
    ②検索用HTMLにユニット、グループ、チーム、ランクのselectボックスを作成
    ③selectボックスに入れるためのoptionを追加するための関数を作成

--- app/assets/javascripts/search_group.js
+++ app/assets/javascripts/search_group.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  // 検索用のHTML
   var buildHTMLGroupSearch = function(){
     // var html = document.createElement("div");
     // html.setAttribute("class", "form_search_group")
@@ -7,15 +9,27 @@ document.addEventListener('DOMContentLoaded', function () {
       <div class="form_top">
         <ul class="search_list">
           <li>
-            <label for="search_group">グループ名</label>
-            <select name="group" id="search_group">
-              <option value=null>グループ名</option>
+            <label for="search_units_list">ユニット名</label>
+            <select name="unit" id="search_units_list" >
+              <option value=null class="search_units_list_add">ユニット名</option>
             </select>
           </li>
           <li>
-            <label for="search_rank">階級名</label>
-            <select name="rank" id="search_rank">
-              <option value=null>階級名</option>
+            <label for="search_groups_list">グループ名</label>
+            <select name="group" id="search_groups_list" >
+              <option value=null class="search_groups_list_add">グループ名</option>
+            </select>
+          </li>
+          <li>
+            <label for="search_teams_list">チーム名</label>
+            <select name="team" id="search_teams_list" >
+              <option value=null class="search_teams_list_add">チーム名</option>
+            </select>
+          </li>
+          <li>
+            <label for="search_rank_list">階級名</label>
+            <select name="rank" id="search_ranks_list" >
+              <option value=null class="search_ranks_list_add">階級名</option>
             </select>
           </li>
           <li>
@@ -30,46 +44,107 @@ document.addEventListener('DOMContentLoaded', function () {
     </div>`
     return html;
   }
+  // ユニット検索用のoption
+  var buildHTMLUnitOption = function (unit) {
+    var option = `<option class="${unit.name}" value="${unit.id}">${unit.name}<option>`;
+    document.getElementsByClassName("search_units_list_add")[0].insertAdjacentHTML("afterend", option);
+    var removeOption = document.getElementsByClassName(unit.name)[0].nextSibling;
+    removeOption.remove();
+  }
+
+  // グループ検索用のoption
+  var buildHTMLGroupOption = function (group) {
+    var option = `<option class="${group.name}" value="${group.id}">${group.name}<option>`;
+    document.getElementsByClassName("search_groups_list_add")[0].insertAdjacentHTML("afterend", option);
+    var removeOption = document.getElementsByClassName(group.name)[0].nextSibling;
+    removeOption.remove();
+  }
+
+  // チーム検索用のoption
+  var buildHTMLTeamOption = function (team) {
+    var option = `<option class="${team.name}" value="${team.id}">${team.name}<option>`;
+    document.getElementsByClassName("search_teams_list_add")[0].insertAdjacentHTML("afterend", option);
+    var removeOption = document.getElementsByClassName(team.name)[0].nextSibling;
+    removeOption.remove();
+  }
+
+  // ランク検索用のoption
+  var buildHTMLRankOption = function (rank) {
+    var option = `<option class="${rank.name}" value="${rank.id}">${rank.name}<option>`;
+    document.getElementsByClassName("search_ranks_list_add")[0].insertAdjacentHTML("afterend", option);
+    var removeOption = document.getElementsByClassName(rank.name)[0].nextSibling;
+    removeOption.remove();
+  }
+
   // グループ検索ボタンを押してイベント発火させる。
   document.getElementById("search_group").addEventListener('click', function(){
-    // htmlを追加
+    // グループ検索用のhtmlを追加
     var html = buildHTMLGroupSearch();
     // afterendはオプションの一つで直後に追加という意味。
     // insertAdjacentHTMLメソッドはjQueryでいうappendメソッド
     document.getElementById("app").insertAdjacentHTML("afterend", html);
 
-    // グループ一覧とランク一覧を取得する。
+    // ユニット一覧を取得する。
     $.ajax({
-
+      url: "/search/units/index",
+      dataType: "json",
+      data: { input: "" }
     })
-    .done()
-    .fail();
+    .done(function (units) {
+      units.forEach(function (unit) {
+        buildHTMLUnitOption(unit);
+      });
+    })
 
+    // グループ一覧を取得する。
+    $.ajax({
+      url: "/search/groups/index",
+      dataType: "json",
+      data: { input: "" }
+    })
+    .done(function(groups){
+      groups.forEach(function(group){
+        buildHTMLGroupOption(group);
+      });
+    })
+
+    // チーム一覧を取得する。
+    $.ajax({
+      url: "/search/teams/index",
+      dataType: "json",
+      data: { input: "" }
+    })
+      .done(function (teams) {
+        teams.forEach(function (team) {
+          buildHTMLTeamOption(team);
+        });
+      })
+
+    // ランク一覧を取得する
+    $.ajax({
+      url: "/search/ranks/index",
+      dataType: "json",
+      data: { input: "" }
+    })
+      .done(function (ranks) {
+        ranks.forEach(function (rank) {
+          buildHTMLRankOption(rank);
+        });
+      })
 
     //グループ検索 
-    document.getElementById("search_group").addEventListener('change', function(){
-      $.ajax()
-      .done()
-      .fail();
-    }, false);
+    // document.getElementById("search_group").addEventListener('change', function(){
+    //   $.ajax()
+    //   .done()
+    //   .fail();
+    // }, false);
 
     // 階級検索
-    document.getElementById("search_rank").addEventListener('change', function () {
-      $.ajax()
-      .done()
-      .fail();
-    }, false);
-
-
-
-
-
-
-
-
-
-
-
+    // document.getElementById("search_rank").addEventListener('change', function () {
+    //   $.ajax()
+    //   .done()
+    //   .fail();
+    // }, false);
 
     // user検索する
     // idが重複して発火しなかったので、idを変更
