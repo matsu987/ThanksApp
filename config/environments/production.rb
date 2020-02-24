53cb30163c58950381e55ab7dff46e5a8040088a
tech-camp <tech-camp@A-393.local>
2020-02-25 00:16:47 +0900

    メール認証を本番環境でも行える様修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,11 +64,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ThanksApp_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'http://18.178.107.5/' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    enable_starttls_auto: true,
+    port: 587,
+    user_name:  Rails.application.credentials.mail[:sender],
+    password: Rails.application.credentials[:app_password],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
