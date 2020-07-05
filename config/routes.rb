ce8f948611583f73cdb4a43f46b40c73b0ed34d7
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2020-07-05 16:18:15 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'thanks#index'
 
-  resources :thanks, except: %i(index, show) do
-    collection do
-      post 'one_time', to: "thanks#one_time_create"
-    end
-  end
+  resources :thanks, except: %i(index, show)
   
   #ユーザー検索
   namespace :search do
