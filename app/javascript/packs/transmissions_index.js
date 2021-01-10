5de60924638b213ec41d23a9cb1573ec83eecb4f
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2021-01-11 03:59:06 +0900

    Update transmissions_index.js

--- app/javascript/packs/transmissions_index.js
+++ app/javascript/packs/transmissions_index.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import TransmissionsIndex from '../TransmissionsIndex.vue'
+// import Vue from 'vue'
+// import TransmissionsIndex from '../TransmissionsIndex.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const transmissionsIndex = new Vue({
-    render: h => h(TransmissionsIndex)
-  }).$mount()
-  document.body.appendChild(transmissionsIndex.$el)
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   const transmissionsIndex = new Vue({
+//     render: h => h(TransmissionsIndex)
+//   }).$mount()
+//   document.body.appendChild(transmissionsIndex.$el)
+// })
