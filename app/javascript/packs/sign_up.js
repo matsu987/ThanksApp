0a599d70275563587b8305b1941f3ac869217a62
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2021-01-11 04:02:19 +0900

    Update sign_up.js

--- app/javascript/packs/sign_up.js
+++ app/javascript/packs/sign_up.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import SignUp from '../components/sign_up/SignUp.vue'
+// import Vue from 'vue'
+// import SignUp from '../components/sign_up/SignUp.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const sign_up = new Vue({
-    render: h => h(SignUp)
-  }).$mount()
-  document.body.appendChild(sign_up.$el)
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   const sign_up = new Vue({
+//     render: h => h(SignUp)
+//   }).$mount()
+//   document.body.appendChild(sign_up.$el)
+// })
