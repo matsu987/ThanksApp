b41390a2387eb2e2b7621a4fc71cee79b816edca
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2021-01-11 17:52:37 +0900

    エラーメッセージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,35 +4,23 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   if !Rails.env.development?
-    rescue_from Exception,                        with: :render_500
-    rescue_from ActiveRecord::RecordNotFound,     with: :render_404
-    rescue_from ActionController::RoutingError,   with: :render_404
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from Exception, with: :render_500
   end
 
-  def routing_error
-    raise ActionController::RoutingError.new(params[:path])
-  end
-
-  def render_404(e = nil)
-    logger.info "Rendering 404 with exception: #{e.message}" if e
-
-    if request.xhr?
-      render json: { error: '404 error' }, status: 404
-    else
-      format = params[:format] == :json ? :json : :html
-      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+  def render_404(exception = nil)
+    if exception
+      logger.info "Rendering 404 with exception: #{exception.message}"
     end
+    render template: "errors/error_404", status: 404, layout: 'application'
   end
 
-  def render_500(e = nil)
-    logger.info "Rendering 500 with exception: #{e.message}" if e 
-
-    if request.xhr?
-      render json: { error: '500 error' }, status: 500
-    else
-      format = params[:format] == :json ? :json : :html
-      render file: Rails.root.join('public/500.html'), status: 500, layout: false, content_type: 'text/html'
+  def render_500(exception = nil)
+    if exception
+      logger.info "Rendering 500 with exception: #{exception.message}"
     end
+    render template: "errors/error_500", status: 500, layout: 'application'
   end
 
   protected
