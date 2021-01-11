308758a5359614a76566cd0506b5add3782b1349
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2021-01-11 20:51:40 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ThanksApp_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'http://18.178.107.5/' }
+  config.action_mailer.default_url_options = { host: 'https://ariletter.com' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
