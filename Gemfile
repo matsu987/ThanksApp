b02640791758368ae12a3f56bbd430be839c10af
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2021-01-11 19:00:56 +0900

    修正

--- Gemfile
+++ Gemfile
@@ -85,4 +85,3 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'fog-aws'
 gem 'whenever', :require => false
-gem 'turnout'
--- config/deploy.rb
+++ config/deploy.rb
@@ -93,23 +93,3 @@ end
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-namespace :maintenance do
-  desc 'start maintenance'
-  task :on do
-    on roles(:web) do
-      target_dir = "#{shared_path}/public/tmp"
-      target_path = "#{target_dir}/maintenance.html"
-      source_path = "#{release_path}/public/maintenance.html"
-      execute :mkdir, '-p', target_dir
-      execute :cp, '-f', source_path, target_path
-    end
-  end
-
-  desc 'stop maintenance'
-  task :off do
-    on roles(:web) do
-      target = "#{shared_path}/public/tmp/maintenance.html"
-      execute :rm, target if test "[ -f #{target} ]"
-    end
-  end
-end
